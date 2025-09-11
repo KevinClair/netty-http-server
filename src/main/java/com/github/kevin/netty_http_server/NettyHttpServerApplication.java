95f5678531fca8cd67400d4ed3061c7095bd4846
KevinClair <kevinclair@apache.org>
2025-09-12 01:00:47 +0800

    Initial commit

---
+++ src/main/java/com/github/kevin/netty_http_server/NettyHttpServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.kevin.netty_http_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyHttpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyHttpServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/kevin/netty_http_server/NettyHttpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kevin.netty_http_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyHttpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
