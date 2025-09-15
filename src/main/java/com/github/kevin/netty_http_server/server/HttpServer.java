d3561ba847e80ffd6dce4439c28e476470881567
KevinClair <kevinclair@apache.org>
2025-09-16 00:01:21 +0800

    feat: 配置Http服务器

---
+++ src/main/java/com/github/kevin/netty_http_server/server/HttpServer.java
@@ -0,0 +1,53 @@
+package com.github.kevin.netty_http_server.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class HttpServer {
+
+    public HttpServer(HttpServerProperties properties) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            // start server
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel channel) throws Exception {
+                            channel.pipeline()
+                                    .addLast(new LoggingHandler(LogLevel.DEBUG))
+                                    .addLast(new HttpServerCodec())
+                                    .addLast(new HttpObjectAggregator(5 * 1024 * 1024))  // merge request & reponse to FULL
+                                    // 添加处理器
+                                    .addLast();
+                        }
+                    })
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            // bind
+            ChannelFuture channelFuture = bootstrap.bind(properties.getPort()).sync();
+
+            channelFuture.channel().closeFuture().addListener(future -> {
+                // close server
+                bossGroup.shutdownGracefully();
+                workerGroup.shutdownGracefully();
+            });
+        } catch (Exception e) {
+            log.error("HttpServer start error", e);
+        }
+    }
+}
