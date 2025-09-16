8cd3e0b3052b8d6bf41a3ee160610a30e41a9448
KevinClair <kevinclair@apache.org>
2025-09-17 00:07:43 +0800

    feat: 添加枚举类

---
+++ src/main/java/com/github/kevin/netty_http_server/handlers/enums/RequestMethod.java
@@ -0,0 +1,15 @@
+package com.github.kevin.netty_http_server.handlers.enums;
+
+/**
+ * 请求方式枚举
+ */
+public enum RequestMethod {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    HEAD,
+    OPTIONS,
+    PATCH,
+    TRACE;
+}
