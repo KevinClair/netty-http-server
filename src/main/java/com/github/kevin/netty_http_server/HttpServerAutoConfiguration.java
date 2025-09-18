ecf1453cd075e8df46a83f2b0470a21004790903
KevinClair <kevinclair@apache.org>
2025-09-18 23:32:43 +0800

    feat: 调整注入配置

--- src/main/java/com/github/kevin/netty_http_server/HttpServerAutoConfiguration.java
+++ src/main/java/com/github/kevin/netty_http_server/HttpServerAutoConfiguration.java
@@ -1,15 +1,15 @@
 package com.github.kevin.netty_http_server;
 
+import com.github.kevin.netty_http_server.handlers.RequestHandlerFactory;
 import com.github.kevin.netty_http_server.server.HttpServer;
 import com.github.kevin.netty_http_server.server.HttpServerProperties;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @ConditionalOnProperty(prefix = "http-server", name = "enable", havingValue = "true")
 @EnableConfigurationProperties(HttpServerProperties.class)
-public class NettyHttpServerAutoConfiguration {
+public class HttpServerAutoConfiguration {
 
     /**
      * Http服务器
@@ -18,7 +18,12 @@ public class NettyHttpServerAutoConfiguration {
      * @return
      */
     @Bean
-    public HttpServer httpServer(HttpServerProperties properties) {
-        return new HttpServer(properties);
+    public HttpServer httpServer(HttpServerProperties properties, RequestHandlerFactory requestHandlerFactory) {
+        return new HttpServer(properties, requestHandlerFactory);
+    }
+
+    @Bean
+    public RequestHandlerFactory requestHandlerFactory() {
+        return new RequestHandlerFactory();
     }
 }
