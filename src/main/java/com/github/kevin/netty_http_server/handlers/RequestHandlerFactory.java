5cd82e2c3f14aebdc514243d7ea6fe59dbb34ab0
KevinClair <kevinclair@apache.org>
2025-09-18 23:32:07 +0800

    feat: 增加注解类解析

---
+++ src/main/java/com/github/kevin/netty_http_server/handlers/RequestHandlerFactory.java
@@ -0,0 +1,57 @@
+package com.github.kevin.netty_http_server.handlers;
+
+import com.github.kevin.netty_http_server.common.HttpServerException;
+import com.github.kevin.netty_http_server.handlers.annotation.HttpServerRequestController;
+import com.github.kevin.netty_http_server.handlers.annotation.HttpServerRequestMapping;
+import com.github.kevin.netty_http_server.handlers.enums.RequestMethod;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+public class RequestHandlerFactory implements ApplicationListener<ApplicationReadyEvent> {
+
+    private Map<String, RequestHandler> handlerMap = new HashMap<>();
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        ApplicationContext applicationContext = event.getApplicationContext();
+        // 扫描所有包含HttpServerRequestController注解的Bean
+        applicationContext.getBeansWithAnnotation(HttpServerRequestController.class).forEach((name, bean) -> {
+            HttpServerRequestController annotation = bean.getClass().getAnnotation(HttpServerRequestController.class);
+            String parentPath = annotation.path().startsWith("/") ? annotation.path() : "/" + annotation.path();
+            // 扫描类中包含所有HttpServerRequestMapping注解的方法
+            Method[] methods = bean.getClass().getMethods();
+            for (Method method : methods) {
+                if (method.isAnnotationPresent(HttpServerRequestMapping.class)) {
+                    HttpServerRequestMapping mapping = method.getAnnotation(HttpServerRequestMapping.class);
+                    String fullPath = parentPath + (mapping.path().startsWith("/") ? mapping.path() : "/" + mapping.path());
+                    // 解析请求方法
+                    for (RequestMethod requestMethod : mapping.method()) {
+                        String requestHandlerKey = requestMethod.name() + ":" + fullPath;
+                        if (handlerMap.containsKey(requestHandlerKey)) {
+                            throw new HttpServerException("Duplicate request mapping: " + requestHandlerKey);
+                        }
+                        handlerMap.put(requestHandlerKey, new RequestHandler(bean, method));
+                    }
+                }
+            }
+        });
+    }
+
+    /**
+     * 获取请求处理器
+     *
+     * @param requestHandlerKey 请求处理器Key，格式为：请求方法:路径，例如 GET:/api/test
+     * @return
+     * @throws HttpServerException
+     */
+    public RequestHandler getRequestHandler(String requestHandlerKey) {
+        return handlerMap.get(requestHandlerKey);
+    }
+}
