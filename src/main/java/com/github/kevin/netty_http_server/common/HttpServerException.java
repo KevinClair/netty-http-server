388a394249d51adda39fa1ea263ff93036180b7e
KevinClair <kevinclair@apache.org>
2025-09-18 23:32:30 +0800

    feat: 异常类

---
+++ src/main/java/com/github/kevin/netty_http_server/common/HttpServerException.java
@@ -0,0 +1,8 @@
+package com.github.kevin.netty_http_server.common;
+
+public class HttpServerException extends RuntimeException {
+
+    public HttpServerException(String message) {
+        super(message);
+    }
+}
