e82f38723473256dbc704b579c660ea51352fb7e
KevinClair <kevinclair@apache.org>
2025-09-24 22:20:31 +0800

    feat: 完善解析请求参数逻辑

--- src/main/java/com/github/kevin/netty_http_server/server/HttpServerHandler.java
+++ src/main/java/com/github/kevin/netty_http_server/server/HttpServerHandler.java
@@ -1,7 +1,10 @@
 package com.github.kevin.netty_http_server.server;
 
 import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONException;
+import com.github.kevin.netty_http_server.common.BaseResponse;
 import com.github.kevin.netty_http_server.common.HttpServerUtils;
+import com.github.kevin.netty_http_server.handlers.ParameterObjects;
 import com.github.kevin.netty_http_server.handlers.RequestHandler;
 import com.github.kevin.netty_http_server.handlers.RequestHandlerFactory;
 import io.netty.buffer.Unpooled;
@@ -10,11 +13,16 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.CollectionUtils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @ChannelHandler.Sharable
 @RequiredArgsConstructor
+@Slf4j
 public class HttpServerHandler extends io.netty.channel.SimpleChannelInboundHandler<io.netty.handler.codec.http.FullHttpRequest> {
 
     private final RequestHandlerFactory requestHandlerFactory;
@@ -33,17 +41,47 @@ public class HttpServerHandler extends io.netty.channel.SimpleChannelInboundHand
         RequestHandler requestHandler = requestHandlerFactory.getRequestHandler(requestHandlerKey);
         if (Objects.isNull(requestHandler)) {
             // 返回 path not found
-
+            this.sendResponse(ctx, msg, JSON.toJSONString(BaseResponse.builder().code(301).msg("request path error").data("Path not found: " + requestHandlerKey).build()));
         }
 
         Object invokeResponse = null;
-        if (requestHandler.getArgs().length == 0) {
+        if (CollectionUtils.isEmpty(requestHandler.getParameterObjects())) {
             invokeResponse = requestHandler.getMethod().invoke(requestHandler.getBean());
         } else {
-            // todo 解析参数
+            List<Object> arguments = new ArrayList<>();
+            for (ParameterObjects parameterObject : requestHandler.getParameterObjects()) {
+                switch (parameterObject.getParameterType()) {
+                    case REQUEST_BODY -> {
+                        if (requestData == null || requestData.isEmpty()) {
+                            if (parameterObject.getRequired()) {
+                                // 返回 参数缺失
+                                this.sendResponse(ctx, msg, JSON.toJSONString(BaseResponse.builder().msg("invalid parameter error").code(302).data("Missing required parameter: " + parameterObject.getParameterClass().getName()).build()));
+                                return;
+                            }
+                            arguments.add(null);
+                        } else {
+                            try {
+                                // 处理JSON反序列化异常，例如传递的不是json类型的数据
+                                arguments.add(JSON.parseObject(requestData, parameterObject.getParameterClass()));
+                            } catch (JSONException exception) {
+                                this.sendResponse(ctx, msg, JSON.toJSONString(BaseResponse.builder().msg("invalid parameter error").code(303).data("Invalid parameter format, JSON parse error for path: " + requestHandlerKey).build()));
+                                return;
+                            }
+                        }
+                    }
+                    case REQUEST_PATH -> log.info("");
+                    case REQUEST_PARAM -> log.info("");
+                }
+
+            }
+            invokeResponse = requestHandler.getMethod().invoke(requestHandler.getBean(), arguments.toArray());
         }
+        this.sendResponse(ctx, msg, JSON.toJSONString(invokeResponse));
+    }
+
+    private void sendResponse(ChannelHandlerContext ctx, FullHttpRequest msg, String responseData) {
         // write response
-        FullHttpResponse fullHttpResponse = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, Unpooled.copiedBuffer(JSON.toJSONString(invokeResponse), CharsetUtil.UTF_8));   //  Unpooled.wrappedBuffer(responseJson)
+        FullHttpResponse fullHttpResponse = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, Unpooled.copiedBuffer(responseData, CharsetUtil.UTF_8));   //  Unpooled.wrappedBuffer(responseJson)
         fullHttpResponse.headers().set(HttpHeaderNames.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON);
         fullHttpResponse.headers().set(HttpHeaderNames.CONTENT_LENGTH, fullHttpResponse.content().readableBytes());
         if (HttpUtil.isKeepAlive(msg)) {
